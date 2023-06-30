ee83c3045b2c6f9fd23cb1070eb9edbc7188ae79
JaySan169 <sanjay01061990@gmail.com>
2023-06-30 19:49:28 +0530

    1st

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -6,7 +6,7 @@ def call() {
         env.ARGS="-Dsonar.java.binaries=target/"
         common.sonarChecks()   
         common.testCases()
-        env.SONARURL = "172.31.0.59"
+        env.SONARURL = "18.205.150.116"
         env.NEXUSURL = "172.31.2.247"
         if(env.TAG_NAME != null ) {
             common.artifacts()
