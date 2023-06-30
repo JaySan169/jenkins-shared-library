070aae716890bfb595bc2a47febd3afe3919a676
JaySan169 <sanjay01061990@gmail.com>
2023-06-30 20:30:05 +0530

    1st

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,8 +1,8 @@
 def sonarChecks() {
    stage('Sonar Checks') {
         sh "echo Starting Code Quality Analysis"
-        sh "sonar-scanner -Dsonar.host.url=http://${SONARURL}:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} ${ARGS}"
-        sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONARURL} ${COMPONENT}" 
+        sh "sonar-scanner -Dsonar.host.url=http://172.31.92.95:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} ${ARGS}"
+        sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.92.95 ${COMPONENT}" 
         sh "echo Code Quality Analysis is Completed"
    }
 }
